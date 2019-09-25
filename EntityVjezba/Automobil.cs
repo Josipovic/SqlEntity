0b49484131407ed3fb222e67fcadee9cc9a121c7
Katarina Josipovic <katarina.josipovic@hotmail.com>
2019-09-25 18:22:29 +0200

    first commit

---
+++ EntityVjezba/Automobil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityVjezba
+{
+    class Automobil
+    {
+        public int Id  { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Godiste { get; set; }
+        public int PredjeniKilometri { get; set; }
+        public int Cijena { get; set; }
+    }
+}
---
+++ EntityVjezba/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityVjezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutomobilContext db = new AutomobilContext();
+
+            foreach (var a in db.Automobili) {
+
+                Console.WriteLine(a.Cijena + " " + a.Marka + " " + a.Model + " " +a.Godiste + " " + a.PredjeniKilometri);
+            }
+
+            Console.Read();
+        }
+    }
+}
